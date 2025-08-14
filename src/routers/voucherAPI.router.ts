803d83d4efbee847f416ff8019c82d0ce27b09ce
ngothanhtien <ngothanhtien1406@gmail.com>
2025-08-14 14:45:04 +0700

    feat: send voucher to email customer

--- src/routers/voucherAPI.router.ts
+++ src/routers/voucherAPI.router.ts
@@ -4,9 +4,10 @@ import { requireAuth } from '@/middlewares/auth.middlewares'
 
 const router = express.Router()
 
-router.post('/', requireAuth, voucherController.createVoucher)
-router.get('/', requireAuth, voucherController.getAllVoucher)
+router.post('/',requireAuth, voucherController.createVoucher)
+router.get('/',requireAuth, voucherController.getAllVoucher)
 router.put('/:id', requireAuth, voucherController.updateVoucher)
 router.delete('/:id', requireAuth, voucherController.deleteVoucher)
+router.post('/code', voucherController.sendVoucher)
 
 export const routerVoucher = router
