d9fd6b9dc653a04e4a11ba658c998293dd0c9820
PhanThanhDat16 <phandat1662004@gmail.com>
2025-09-15 09:03:53 +0700

    fix: fix position router item

--- src/routers/itemAPI.router.ts
+++ src/routers/itemAPI.router.ts
@@ -3,11 +3,11 @@ import { itemController } from '@/controllers/item/item.controller'
 import { requireAuth } from '@/middlewares/auth.middlewares'
 const router = express.Router()
 
-router.post('/', requireAuth, itemController.createItem)
-router.get('/:id', requireAuth, itemController.getItemDetail)
 router.get('/', itemController.getAllItem)
 router.get('/popular', itemController.getItemTopPopular)
 router.get('/category/:id', itemController.getAllItemByCategoryId)
+router.post('/', requireAuth, itemController.createItem)
+router.get('/:id', requireAuth, itemController.getItemDetail)
 router.delete('/:id', requireAuth, itemController.deleteItem)
 router.put('/:id', requireAuth, itemController.updateItem)
 
